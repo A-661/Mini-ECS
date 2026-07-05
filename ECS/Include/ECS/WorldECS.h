30b33f97fd1b8da610a00cfe7b8edddceba74e32
Andrey Suvorov <andryusha.suvorov.05@mail.ru>
2026-07-05 17:16:31 +0300

    Adding names to entities

--- ECS/Include/ECS/WorldECS.h
+++ ECS/Include/ECS/WorldECS.h
@@ -3,9 +3,11 @@
 #include "ECS/Components/TransformComponent.h"
 #include "ECS/Components/VelocityComponent.h"
 #include "ECS/Components/HealthComponent.h"
+#include "ECS/Components/NameComponent.h"
 
 using WorldECS = ECSStorage<
     TransformComponent,
     VelocityComponent,
-    HealthComponent
+    HealthComponent,
+    NameComponent
 >;
